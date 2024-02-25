39e2c23478f8878c7e037ba93bbb54bd08aef2d4
edanur <kutluayedanur@gmail.com>
2024-02-25 14:04:20 +0300

    Add project files.

---
+++ KodlamaCrud/Business/Abstract/ICourseService.cs
@@ -0,0 +1,17 @@
+﻿using KodlamaCrud.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaCrud.Business.Abstract
+{
+    internal interface ICourseService
+    {
+        List<Course> GetAll();
+        void Add(Course course);
+        void Update(Course course);
+        void Delete(int id);
+    }
+}
---
+++ KodlamaCrud/DataAccess/Abstract/IInstructorDal.cs
@@ -0,0 +1,14 @@
+﻿using KodlamaCrud.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaCrud.DataAccess.Abstract
+{
+    public interface IInstructorDal : IEntityRepository<Instructor>
+    {
+        void Add(Instructor instructor);
+    }
+}
